ceed4d71bddf3094f9adc2f7b7dce0b03518939e
Ethan Mosbaugh <ethan@replicated.com>
2021-10-22 16:38:48 +0000

    label owner rather than prefix

--- .gcommands.sh
+++ .gcommands.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# GPREFIX=
-# GZONE=
+# GUSER=
 
 glist() {
-  gcloud compute instances list | grep "^${GPREFIX}"
+  gcloud compute instances list --filter="labels.owner:${GUSER}"
 }
 
 gcreate() {
@@ -21,8 +20,9 @@ gcreate() {
   local default_service_account
   default_service_account="$(gcloud iam service-accounts list | grep '\-compute@developer.gserviceaccount.com' | awk 'BEGIN {FS="  "}; {print $2}')"
   shift
-  (set -x; gcloud compute --project=replicated-qa instances create $(echo $@ | sed "s/[^ ]* */${GPREFIX}&/g") \
-    "--zone=${ZONE:-us-central1-a}" --machine-type=n1-standard-4 \
+  (set -x; gcloud compute instances create $(echo $@) \
+    --labels owner="${GUSER}" \
+    --machine-type=n1-standard-4 \
     --subnet=default --network-tier=PREMIUM --maintenance-policy=MIGRATE \
     --service-account="${default_service_account}" \
     --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append \
@@ -32,10 +32,10 @@ gcreate() {
 }
 
 gdelete() {
-  local usage="Usage: gdelete [INSTANCE_NAME_PREFIX]"
-  local instance_name_prefix="${GPREFIX}$1"
-  if ! gcloud compute instances list | awk '{if(NR>1)print}' | grep RUNNING | grep -q "^$instance_name_prefix" ; then echo "no instances match pattern \"^$instance_name_prefix\""; echo "${usage}" return 1; fi
-  gcloud compute instances delete --delete-disks=all $(gcloud compute instances list | awk '{if(NR>1)print}' | grep RUNNING | grep "^$instance_name_prefix" | awk '{print $1}' | xargs echo)
+  local usage="Usage: gdelete [INSTANCE_NAMES]"
+  local instance_name="$1"
+  if ! gcloud compute instances list --filter="labels.owner:${GUSER}" | awk '{if(NR>1)print}' | grep RUNNING | grep -q "^$instance_name" ; then echo "no instances match pattern \"^$instance_name\""; echo "${usage}" return 1; fi
+  gcloud compute instances delete --delete-disks=all $(gcloud compute instances list --filter="labels.owner:${GUSER}" | awk '{if(NR>1)print}' | grep RUNNING | grep "^$instance_name" | awk '{print $1}' | xargs echo)
 }
 
 gonline() {
@@ -43,8 +43,8 @@ gonline() {
   if [ "$#" -lt 1 ]; then echo "${usage}"; return 1; fi
   local instance
   for instance in "$@"; do
-    local instance_name_prefix="${GPREFIX}${instance}"
-    (set -x; gcloud compute instances add-access-config "${instance_name_prefix}" --access-config-name="external-nat")
+    local instance_name="${instance}"
+    (set -x; gcloud compute instances add-access-config "${instance_name}" --access-config-name="external-nat")
   done
 }
 
@@ -53,10 +53,10 @@ gairgap() {
   if [ "$#" -lt 1 ]; then echo "${usage}"; return 1; fi
   local instance
   for instance in "$@"; do
-    local instance_name_prefix="${GPREFIX}${instance}"
+    local instance_name="${instance}"
     local access_config_name
-    access_config_name="$(gcloud compute instances describe "${instance_name_prefix}" --format="value(networkInterfaces[0].accessConfigs[0].name)")"
-    (set -x; gcloud compute instances delete-access-config "${instance_name_prefix}" --access-config-name="${access_config_name}")
+    access_config_name="$(gcloud compute instances describe "${instance_name}" --format="value(networkInterfaces[0].accessConfigs[0].name)")"
+    (set -x; gcloud compute instances delete-access-config "${instance_name}" --access-config-name="${access_config_name}")
   done
 }
 
@@ -78,23 +78,24 @@ gssh() {
 gdisk() {
   local usage="Usage: gdisk [DISK_NAMES]"
   if [ "$#" -lt 1 ]; then echo "${usage}"; return 1; fi
-  (set -x; gcloud compute disks create $(echo $@ | sed "s/[^ ]* */${GPREFIX}disk-&/g") \
-    --type=pd-balanced --size=100GB --zone="${GZONE}")
+  (set -x; gcloud compute disks create $(echo $@ | sed "s/[^ ]* */disk-&/g") \
+    --labels owner="${GUSER}" \
+    --type=pd-balanced --size=100GB)
 }
 
 gattach() {
   local usage="Usage: gattach [INSTANCE_NAME] [DISK_NAME]"
   if [ "$#" -ne 2 ]; then echo "${usage}"; return 1; fi
-  local instance_name_prefix="${GPREFIX}$1"
-  local disk_name_prefix="${GPREFIX}disk-$2"
-  local device_name_prefix="${GPREFIX}$1-disk-$2"
-  (set -x; gcloud compute instances attach-disk "${instance_name_prefix}" --disk="${disk_name_prefix}" --device-name="${device_name_prefix}")
+  local instance_name="$1"
+  local disk_name="disk-$2"
+  local device_name="$1-disk-$2"
+  (set -x; gcloud compute instances attach-disk "${instance_name}" --disk="${disk_name}" --device-name="${device_name}")
 }
 
 gtag() {
   local usage="Usage: gattach [INSTANCE_NAME] [comma-delimited list of TAGS]"
   if [ "$#" -ne 2 ]; then echo "${usage}"; return 1; fi
-  local instance_name_prefix="${GPREFIX}$1"
+  local instance_name="$1"
   local tags="$2"
-  (set -x; gcloud compute instances add-tags "${instance_name_prefix}" --tags="${tags}" --zone="${GZONE}")
+  (set -x; gcloud compute instances add-tags "${instance_name}" --tags="${tags}")
 }
--- README.md
+++ README.md
@@ -7,7 +7,9 @@ Some commands to make working with gcloud easier.
 Add the following to your `.profile`:
 
 ```
-export GPREFIX=ethan-
-export GZONE=us-central1-a
+export CLOUDSDK_CORE_PROJECT=replicated-qa
+exoprt CLOUDSDK_COMPUTE_REGION=us-central1
+export CLOUDSDK_COMPUTE_ZONE=us-central1-c
+export GUSER=ethan
 source $HOME/.gcommands.sh
 ```
